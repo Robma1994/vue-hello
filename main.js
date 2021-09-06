964831f56083f5b753bce1bed904319583f1dcfe
Roberto <robertomartinonwg@gmail.com>
2021-09-06 15:33:10 +0200

    Esercizio completato

---
+++ main.js
@@ -0,0 +1,12 @@
+/*Stampare a schermo un messaggio all’interno di un h1, 
+utilizzando i data.
+
+*/
+
+
+let root = new Vue({
+        el: "#root",
+        data: {
+            message: "Forza Napoli"
+        }
+});
